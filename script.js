bc83157e7ae4e7e76711f89de717d51d81f3fb53
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-31 17:00:02 +0200

    Update

--- script.js
+++ script.js
@@ -55,25 +55,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movemets){
-  movements.forEach(function(mov, i) {
+const displayMovements = function(movements){
+  containerMovements.innerHTML = ''
+
+   movements.forEach(function(mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
+
     const html = `
       <div class="movements__row">
-        <div class="movements__type
-        movement__type--deposit">${i + 1}</div>
+        <div class="movements__type movement__type--${type}">${i + 1} ${type}</div>
         <div class="movements__value">${mov}</div>
-      </div>
-    `
+      </div>`
+    containerMovements.insertAdjacentHTML('afterbegin', html)
   })
 }
 
 displayMovements(account1.movements)
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
+/*const currencies = new Map([
+  ['USD', 'United States dollar'],  
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
-]);
+]);*/
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+///const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
